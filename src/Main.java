020f1210f82ce3b0aa5ffa5ad2c73a756acea368
Neto test <audov@mail.ru>
2023-04-28 17:32:19 +0300

    Main.java updated

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,5 @@ public class Main {
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ки/ка) специй");
 
-    }   
+    }
 }
